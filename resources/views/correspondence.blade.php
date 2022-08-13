db8e2beb8c8ea712a8ab7bb5e49c0d3701b4383a
itnia <itnia.aurey@outlook.com>
2022-08-14 00:42:44 +0300

    new 2

---
+++ resources/views/correspondence.blade.php
@@ -0,0 +1,8 @@
+<h2>correspondence</h2>
+<button>Написать сообщение</button>
+<div>
+    Выбрать людей через поиск<br>
+    Написать сообщение<br>
+    Сохронить переписку<br>
+    Переписка - /correspondence/id
+</div>
